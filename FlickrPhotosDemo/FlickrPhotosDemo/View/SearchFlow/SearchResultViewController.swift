77c2ac808a393e35e9ab187cc1ee5042d4129118
Roy Sparrow <rvxl2013@gmail.com>
2020-04-14 23:27:50 +0800

    [add] create SearchResultViewController and Storyboard

---
+++ FlickrPhotosDemo/FlickrPhotosDemo/View/SearchFlow/SearchResultViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResultViewController.swift
+//  FlickrPhotosDemo
+//
+//  Created by Roy Sparrow on 2020/4/14.
+//  Copyright © 2020 SparrowStudio. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    deinit {
+        Logger.log(message: "已釋放")
+    }
+}
